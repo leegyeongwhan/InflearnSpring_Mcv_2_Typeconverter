1fefeb75380377b06995a032a1a987b9c7c99032
leegyeongwhan <leekhy02@naver.com>
2022-11-21 11:29:05 +0900

    feat(인프런 스프링 mvc2편): 컨버터 활용
    Stirng -> Integer , Interger -> String , 사용자타입 -> Stirng 등 다양한
    방법으로
    Converter의 기본적인 활용

---
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,40 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConverterTest {
+
+    @Test
+    void stringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer convert = converter.convert("10");
+        assertThat(convert).isEqualTo(10);
+    }
+
+    @Test
+    void IntegerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String convert = converter.convert(10);
+        assertThat(convert).isEqualTo("10");
+    }
+
+    @Test
+    void stringToIpPort() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(ipPort);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void ipPortToIpString() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String ipPort = "127.0.0.1:8080";
+        IpPort convert = converter.convert(ipPort);
+        assertThat(convert).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+}
